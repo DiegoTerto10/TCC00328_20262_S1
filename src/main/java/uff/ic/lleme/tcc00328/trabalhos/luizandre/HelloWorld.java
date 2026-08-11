705104c33ba60ac6ae6ebf583a9c3f304e0ad694
Luiz André Portes Paes Leme <lapaesleme@ic.uff.br>
2026-08-11 16:13:20 -0300

    Update HelloWorld.java

--- src/main/java/uff/ic/lleme/tcc00328/trabalhos/luizandre/HelloWorld.java
+++ src/main/java/uff/ic/lleme/tcc00328/trabalhos/luizandre/HelloWorld.java
@@ -2,8 +2,16 @@ package uff.ic.lleme.tcc00328.trabalhos.luizandre;
 
 public class HelloWorld {
 
+    public static double n;
+    public static char[] c = new char[200];
+    public static String[] str = new String[200];
+    public static double[] vet = new double[200];
+
     // program startup function
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (int i = 0; i < str.length; i++)
+            str[i] = "";
+        System.out.println(str);
+
     }
 }
