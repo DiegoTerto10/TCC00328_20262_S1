19d8fe785528c731332f63f16c941d237c388a44
Luiz André Portes Paes Leme <lapaesleme@ic.uff.br>
2026-08-24 10:55:14 -0300

    Update Histograma.java

--- src/main/java/uff/ic/lleme/tcc00328/trabalhos/luizandre/ex1/Histograma.java
+++ src/main/java/uff/ic/lleme/tcc00328/trabalhos/luizandre/ex1/Histograma.java
@@ -5,38 +5,38 @@ import java.util.Random;
 
 public class Histograma {
 
-    public static int N = 100;
-    public static int[] vet = new int[1000];
-    public static double[] freq = new double[N];
+    public static final int N = 1000;
+    public static final int MAX = 99;
     public static Random rnd = new Random();
+
+    public static int[] numeros = new int[N];
+    public static double[] freq = new double[N];
 
     public static void main(String[] args) {
 
-        vet = geraNumeros(N);
+        numeros = geraNumeros(N, MAX);
+        System.out.println(Arrays.toString(numeros));
 
-        System.out.println(Arrays.toString(vet));
-
-        freq = histograma(vet);
-
+        freq = calculaHistograma(numeros);
         System.out.println(Arrays.toString(freq));
     }
 
-    public static int[] geraNumeros(int N) {
-        int[] out = new int[N];
-        for (int i = 0; i < vet.length; i++)
-            vet[i] = rnd.nextInt(0, 100);
-        return out;
+    public static int[] geraNumeros(int N, int max) {
+        int[] numeros = new int[N];
+        for (int i = 0; i < numeros.length; i++)
+            numeros[i] = rnd.nextInt(0, max + 1);
+        return numeros;
     }
 
-    public static double[] histograma(int[] in) {
-        int[] contador = new int[N];
-        double[] out = new double[N];
+    public static double[] calculaHistograma(int[] in) {
+        int[] contador = new int[in.length];
+        double[] freq = new double[in.length];
 
-        for (int i = 0; i < vet.length; i++)
+        for (int i = 0; i < numeros.length; i++)
             contador[in[i]]++;
-        for (int i = 0; i < vet.length; i++)
-            out[i] = contador[i] / (double) in.length * 100;
-        return out;
+        for (int i = 0; i < numeros.length; i++)
+            freq[i] = contador[i] / (double) in.length * 100;
+        return freq;
     }
 
 }
