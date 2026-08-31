0d16cde8f306e82dde74278932850f13c17a48b0
lugralves <93734984+lugralves@users.noreply.github.com>
2026-08-31 12:55:40 -0300

    Add files via upload

---
+++ src/main/java/uff/ic/lleme/tcc00328/trabalhos/LuizaGonzalez/SistemaAcademico/src/main/java/Aluno.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Aluno {
+    public String nome;
+    public String curso;
+    public String matricula;
+    public String email;
+    public String senha;
+    
+    public Aluno(){
+        notas = new Notas();
+    }
+    
+    public void cadastrarAluno(){
+    Scanner sc = new Scanner(System.in);
+    
+    System.out.println("Digite seu nome:");
+    nome = sc.nextLine();
+    }
+}
---
+++ src/main/java/uff/ic/lleme/tcc00328/trabalhos/LuizaGonzalez/SistemaAcademico/src/main/java/Turma.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author aluno
+ */
+public class Turma {
+    public String sigla;
+    public Disciplina disciplina;
+    public String horario;
+    
+    //overload = sobrecarga. permite metodos com mesmo nome mas que tenham parametros diferentes
+}
